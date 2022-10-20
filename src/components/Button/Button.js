fac19a8f70035fc1872a4b1886f1105448e5ca47
olehhapuk <oleggapuk20@gmail.com>
2022-10-20 19:30:09 +0300

    Styled components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,17 @@
-import styles from './Button.module.css';
+import styled from 'styled-components';
 
-export default function Button({ type, children, variant = 'default' }) {
-  return (
-    <button type={type} className={styles[variant]}>
-      {children}
-    </button>
-  );
-}
+const Button = styled.button`
+  padding: 12px 16px;
+  border-radius: 8px;
+  border: none;
+  font-family: inherit;
+  font-size: 14px;
+  background-color: ${({ variant }) =>
+    variant === 'active' ? '#80ff72' : '#ff8772'};
+  color: black;
+  cursor: pointer;
+  border: 1px solid transparent;
+  transition: background-color 150ms linear;
+`;
+
+export default Button;
